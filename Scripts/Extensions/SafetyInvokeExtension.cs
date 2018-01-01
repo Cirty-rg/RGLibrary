5abfb75229fdcca0c96b9650b865c706397992fd
Cirty <cirty.rg@gmail.com>
2018-01-01 21:12:53 +0900

    [update] added functions summary.
    Signed-off-by: Cirty <cirty.rg@gmail.com>

--- Scripts/Extensions/SafetyInvokeExtension.cs
+++ Scripts/Extensions/SafetyInvokeExtension.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public static class SafetyInvokeExtension {
 
-
+    /// <summary>
+    /// Call the method represented by the current delegate.
+    /// <para>If the delegate is null it does not do anything.</para>
+    /// </summary>
+    /// <param name="delegate">delegate</param>
     public static void SafetyInvoke(this Action @delegate)
     {
 #if NET_4_6
@@ -16,6 +20,13 @@ public static class SafetyInvokeExtension {
 #endif
     }
 
+    /// <summary>
+    /// Call the method represented by the current delegate.
+    /// <para>If the delegate is null it does not do anything.</para>
+    /// </summary>
+    /// <typeparam name="TArg">delegate argument type</typeparam>
+    /// <param name="delegate">delegate</param>
+    /// <param name="arg">delegate argument</param>
     public static void SafetyInvoke<TArg>(this Action<TArg> @delegate, TArg arg)
     {
 #if NET_4_6
