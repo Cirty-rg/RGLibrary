5ede0bfd44d2a70e0ef7ce1182f7a77255eeae4d
Cirty <cirty.rg@gmail.com>
2018-01-01 20:53:54 +0900

    [add] Allow null check of Action class to be omitted.
    Added 'SafetyInvke' function which can omit null check as extension method to Action class.
     ・Action.SafetyInvoke();
     ・Action<T>.SafetyInvoke(t);
    
    Signed-off-by: Cirty <cirty.rg@gmail.com>

---
+++ Scripts/Extensions/SafetyInvokeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SafetyInvokeExtension {
+
+
+    public static void SafetyInvoke(this Action @delegate)
+    {
+#if NET_4_6
+        @delegate?.Invoke();
+#else
+        if (@delegate == null) return;
+        @delegate.Invoke();
+#endif
+    }
+
+    public static void SafetyInvoke<TArg>(this Action<TArg> @delegate, TArg arg)
+    {
+#if NET_4_6
+        @delegate?.Invoke(arg);
+#else
+        if (@delegate == null) return;
+        @delegate.Invoke(arg);
+#endif
+    }
+}
